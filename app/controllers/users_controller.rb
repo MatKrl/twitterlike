ae3bcdd49a8e630dfbd4215736ac6bc3b4a3b73b
Mateusz Król <mateusz.krol.mk@gmail.com>
2016-11-18 18:53:25 +0100

    Basic implementation of Devise; Prepared Dashboard

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
+  def dashboard
+    @messages = [
+      OpenStruct.new(content: 'Post numer jeden jakis tam 111'),
+      OpenStruct.new(content: 'Post numer jeden jakis tam 222'),
+      OpenStruct.new(content: 'Post numer jeden jakis tam 333'),
+      OpenStruct.new(content: 'Post numer jeden jakis tam 444')
+    ]
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_user
+      @user = current_user
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :username, :first_name, :last_name)
+    end
+end
