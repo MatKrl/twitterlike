8823ed318bb10d13afbdf6130a5a74ead8b69475
Mateusz Król <mateusz.krol.mk@gmail.com>
2016-11-23 10:40:28 +0100

    Refactored Rspec tests; Removed unused files

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe MessagesController, type: :controller do
-  context 'For logged in user' do
-    before(:each) do
-      @user1 = create(:user1)
-    end
+  let(:user) { create(:user1) }
 
+  context 'For logged in user' do    
+    before { sign_in(user) }
+  
     describe 'POST create with correct params' do
-      it 'create new message' do
-        sign_in(@user1)
+      it 'creates new message' do
         post :create, params: { message: { content: 'New amazing message!' } }
         expect(Message.count).to eq(1)
       end
@@ -16,7 +15,6 @@ RSpec.describe MessagesController, type: :controller do
 
     describe 'POST create with invalid params' do
       it 'shows error info' do
-        sign_in(@user1)
         post :create, params: { message: { content: '' } }
         expect(assigns(:info)).to be_kind_of(String)
         expect(Message.count).to eq(0)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,46 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before(:each) do
-    @user1 = create(:user1)
-    @user2 = create(:user2)
-    @user3 = create(:user3)
-    @user1.friends << @user2
-    @user1.blocked_users << @user3
-  end
+  let(:user1) { create(:user1) }
+  let(:user2) { create(:user2) }
+  let(:user3) { create(:user3) }
 
-  context 'with friend and blockade' do
-    it 'have reversed friendship relation' do
-      expect(@user2.reload.friends).to eq([@user1])
-    end
+  describe "#full_name" do
+    let(:user1) { User.new(first_name: "Andy", last_name: "Black") }
 
-    it 'have reversed blockade relation' do
-      expect(@user3.reload.blocked_by_users).to eq([@user1])
-    end
-
-    it 'return correct users for dashboard' do
-      expect(@user1.reload.for_dashboard_user_ids).to eq([@user2.id, @user1.id])
-    end
-
-    it 'removing connected friendship when unlike' do
-      @user1.friends.destroy(@user2)
-      expect(@user2.reload.friends).to eq([])
+    it 'returns full name' do
+      expect(user1.full_name).to eq('Andy Black')
     end
   end
 
-  it 'return full name' do
-    expect(@user1.reload.full_name).to eq('Andy Black')
+  describe "#all_other_users" do
+    it 'returns all other users' do
+      expect(user1.all_other_users).to eq([user2, user3])
+    end
   end
 
-  it 'return other users' do
-    expect(@user1.reload.all_other_users).to eq([@user2, @user3])
+  describe ".callbacks" do
+    describe "#create_connected_friendship" do
+      before { user1.friends << user2 }
+
+      it 'sets reversed friendship relation' do
+        expect(user2.friends).to eq([user1])
+      end
+    end
   end
 
-  it 'confirm friendship' do
-    expect(@user1.reload.friend_with?(@user2)).to be true
+  describe "#friend_with?" do
+    context "with friends" do
+      before { user1.friends << user2 }
+
+      it 'confirm friendship' do
+        expect(user1.friend_with?(user2)).to be_truthy
+      end
+    end
+
+    context "without friends" do
+      it 'confirm no friendship' do
+        expect(user1.friend_with?(user2)).to be_falsey
+      end
+    end
   end
 
-  it 'confirm blockade' do
-    expect(@user1.reload.blocked?(@user3)).to be true
+  describe "#blocked?" do
+    context "with blocked users" do
+      before { user1.blocked_users << user3 }
+
+      it 'confirm blockade' do
+        expect(user1.blocked?(user3)).to be_truthy
+      end
+    end
+
+    context "without blockade" do
+      it 'confirm no blockade' do
+        expect(user1.blocked?(user3)).to be_falsey
+      end
+    end
+  end
+
+  describe "#not_blocked_friends?" do
+    before do 
+      user1.friends << user2
+      user1.friends << user3
+    end
+    context "with blockade created by current user" do
+      before { user1.blocked_users << user2 }
+      it 'returns not blocked friends' do
+        expect(user1.not_blocked_friends).to eq([user3])
+      end
+    end
+
+    context "with blockade created by other user" do
+      before { user3.blocked_users << user1 }
+      it 'returns friends who not blocked user' do
+        expect(user1.not_blocked_friends).to eq([user2])
+      end
+    end
   end
 end
